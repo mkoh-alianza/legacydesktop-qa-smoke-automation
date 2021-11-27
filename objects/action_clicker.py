94f590fe2b7d163aad826b56032e93c0df586979
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2021-11-26 16:53:05 -0800

    Updated Readme

--- objects/action_clicker.py
+++ objects/action_clicker.py
@@ -3,6 +3,10 @@ from time import sleep
 import os
 
 class ActionClicker:
+    
+    def __init__(self, source):
+        self.totalEnds
+        
     def doAction(action):
         coords =[]
         with open(os.getcwd() + "./actions/"  + action + '.txt', 'r') as f:
@@ -18,3 +22,19 @@ class ActionClicker:
             pyautogui.mouseDown(button=coords[j][3])
             pyautogui.mouseUp(button=coords[j][3])
             
+    def switchToRemote(self, pc):
+        os.getcwd() + './actions/remote.txt', 'r') as f:
+            for line in f:
+                coords.append(eval(line))
+        
+        offset = (self.totalEnds / 2 * 40) + 20
+        
+        
+        pyautogui.moveTo(coords[0][0], coords[0][1])
+        pyautogui.mouseDown(button=coords[0][3])
+        pyautogui.mouseUp(button=coords[0][3])
+        time.sleep(1)
+        
+        pyautogui.moveTo(coords[0][0] - offset + pc * 40, coords[0][1] + 40)
+        pyautogui.mouseDown(button=coords[0][3])
+        pyautogui.mouseUp(button=coords[0][3])
