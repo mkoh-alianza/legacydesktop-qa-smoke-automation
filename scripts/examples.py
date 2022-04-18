8e5d0703b49d22a97a909e135745f23c694144cc
Kyle Robert van Winkoop <kyle.vanwinkoop@alianza.com>
2022-04-18 13:54:10 -0700

    Added example file
    Added a file containing some example test cases

--- scripts/examples.py
+++ scripts/examples.py
@@ -6,4 +6,15 @@ from test_agent import TestAgent
 
 ts = TestAgent()
 
-ts.test_incoming_call()
+#Sets up a Contact Call here by going to contacts, searching up end b, calling them
+do("Contacts")
+do("ContSearch")
+ActionClicker.type(END_B_NAME)
+do("ContactCall1")
+ActionClicker.switchToRemote(NUM_ENDS, 0)
+wait(3)
+do("Answer")
+ActionClicker.backToLocal()
+
+#Now that it is in a call with end B, we can verify audio
+ts.test_audio()
